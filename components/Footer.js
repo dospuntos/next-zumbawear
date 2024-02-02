8ff23a638c2deece2e77f9839083e30784a37b48
Johan Wagenheim <johan@dospuntos.no>
2024-02-02 12:24:34 -0400

    Update footer date

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="absolute bottom-0 w-full bg-gray-100 border-t-2 border-gray-600 py-2">
       <div className="container px-4 leading-9">
-        <p>© 2023 Zumba Fitness, LLC</p>
+        <p>© 2024 Zumba Fitness, LLC</p>
       </div>
     </footer>
   );
